fa7e228f501db33983bb65c2cc011de8c17ed645
Marcelo Ferreira <mah.tf@hotmail.com>
2020-07-20 10:42:12 -0300

    Adicionado Pontuacao e Pontuavel

---
+++ Assets/Scritpt/Gameplay/Pontuacao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour
+{
+    [SerializeField]
+    private Text textoPontuacao;
+    private int pontos;
+
+    public void Pontuar()
+    {
+        pontos++;
+        AtualizarPontuacao();
+    }
+
+    private void AtualizarPontuacao()
+    {
+        textoPontuacao.text = pontos.ToString();
+    }
+}
