324ad0482307c7cecffc1752af5d2415f96d5952
Marcelo Ferreira <mah.tf@hotmail.com>
2020-07-20 11:33:33 -0300

    Editado a tela de pause

---
+++ Assets/Scritpt/Gameplay/ControlePausa.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePausa : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject painelPause;
+    [SerializeField, Range(0, 1)]
+    private float escalaTempoPause;
+    private bool jogoParado;
+    private void Update()
+    {
+        if (TocandoTela())
+        {
+            if (jogoParado)
+            {
+                ContinuarJogo();
+            }
+        }
+        else
+        {
+            if (!jogoParado)
+            {
+                PararJogo();
+            }
+        }
+    }
+
+    private bool TocandoTela()
+    {
+        return Input.touchCount > 0;
+    }
+
+    private void ContinuarJogo()
+    {
+        StartCoroutine(EsperarEContinuar());
+    }
+
+    private IEnumerator EsperarEContinuar()
+    {
+        yield return new WaitForSecondsRealtime(0.2F);
+        painelPause.SetActive(false);
+        MudarEscalaTempo(1);
+        jogoParado = true;
+    }
+
+    private void PararJogo()
+    {
+        painelPause.SetActive(true);
+        MudarEscalaTempo(escalaTempoPause);
+        jogoParado = false;
+    }
+
+    private void MudarEscalaTempo(float escala)
+    {
+        Time.timeScale = escala;
+        Time.fixedDeltaTime = 0.02F * escala;
+    }
+}
