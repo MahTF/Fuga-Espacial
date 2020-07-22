426e5e7331c4a4671874b16f051ee0626c4f6a42
Marcelo Ferreira <mah.tf@hotmail.com>
2020-07-22 04:30:04 -0300

    Melhorando a tela de ranking

--- Assets/Scritpt/Ranking.cs
+++ Assets/Scritpt/Ranking.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using UnityEngine;
 
@@ -8,10 +9,13 @@ public class Ranking : MonoBehaviour
     private static string NOME_ARQUIVO = "Ranking.json";
     [SerializeField]
     private List<int> pontos;
+    private string caminhoArquivo;
 
-    private void Start()
+    private void Awake()
     {
-        pontos = new List<int>();
+        caminhoArquivo = Path.Combine(Application.persistentDataPath, NOME_ARQUIVO);
+        var textoJson = File.ReadAllText(caminhoArquivo);
+        JsonUtility.FromJsonOverwrite(textoJson, this);
     }
 
     public void AdicionarPontuacao(int pontos)
@@ -23,7 +27,11 @@ public class Ranking : MonoBehaviour
     private void SalvarRanking()
     {
         var textoJson = JsonUtility.ToJson(this);
-        var caminhoArquivo = Path.Combine(Application.persistentDataPath, NOME_ARQUIVO);
         File.WriteAllText(caminhoArquivo, textoJson);
+    }
+
+    public ReadOnlyCollection<int> GetPontos()
+    {
+        return pontos.AsReadOnly();
     }
 }
---
+++ Assets/Scritpt/UI/PainelRanking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PainelRanking : MonoBehaviour
+{
+    [SerializeField]
+    private Ranking ranking;
+    [SerializeField]
+    private GameObject prefabColocado;
+
+    private void Start()
+    {
+        var listaPontos = ranking.GetPontos();
+        for (int i = 0; i < listaPontos.Count; i++)
+        {
+            if (i >= 5)
+            {
+                break;
+            }
+
+            var colocado = Instantiate(prefabColocado, transform);
+            colocado.GetComponent<ItemRanking>().Configurar(i + 1, "Nome", listaPontos[i]);
+        }
+    }
+}
