8d7c8745428c4599cc3ed60855964e7a7f249866
Marcelo Ferreira <mah.tf@hotmail.com>
2020-07-22 06:00:53 -0300

    Corrigido bug da pontuacao

---
+++ Assets/Scritpt/UnicaInstancia.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnicaInstancia : MonoBehaviour
+{
+    private void Start()
+    {
+        var outrasInstancias = GameObject.FindGameObjectsWithTag(tag);
+        foreach (var instancia in outrasInstancias)
+        {
+            if(instancia != this.gameObject)
+            {
+                Destroy(instancia.gameObject);
+            }
+        }
+    }
+}
