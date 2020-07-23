318e8e77be22acc2fab92eddb99e81e7434315d6
Marcelo Ferreira <mah.tf@hotmail.com>
2020-07-23 12:52:59 -0300

    Melhorado a leitura de nome na tela de ranking

--- Assets/Scritpt/UI/NovaPontuacao.cs
+++ Assets/Scritpt/UI/NovaPontuacao.cs
@@ -7,11 +7,22 @@ public class NovaPontuacao : MonoBehaviour
     [SerializeField]
     private TextoDinamico textoPontuacao;
     [SerializeField]
+    private TextoDinamico textoNome;
+    [SerializeField]
     private Ranking ranking;
     private Pontuacao pontuacao;
     private string id;
 
     private void Start()
+    {
+        int totalPontos = GetPontuacao();
+        var nomePessoa = GetNome();
+        textoPontuacao.AtualizarTexto(totalPontos);
+        textoNome.AtualizarTexto(nomePessoa);
+        id = ranking.AdicionarPontuacao(totalPontos, nomePessoa);
+    }
+
+    private int GetPontuacao()
     {
         pontuacao = FindObjectOfType<Pontuacao>();
         var totalPontos = -1;
@@ -19,12 +30,22 @@ public class NovaPontuacao : MonoBehaviour
         {
             totalPontos = pontuacao.Pontos;
         }
-        textoPontuacao.AtualizarTexto(totalPontos);
-        id = ranking.AdicionarPontuacao(totalPontos, "Nome");
+        return totalPontos;
+    }
+
+    private string GetNome()
+    {
+        if (PlayerPrefs.HasKey("UltimoNome"))
+        {
+            return PlayerPrefs.GetString("UltimoNome");
+        }
+        return "Nome";
     }
 
     public void AlterarNome(string nome)
     {
         ranking.AlterarNome(nome, id);
+        PlayerPrefs.SetString("UltimoNome", nome);
     }
+
 }
